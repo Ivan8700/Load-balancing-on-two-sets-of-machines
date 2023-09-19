70a4c450b0911a68d4b337296cc22cea3a3ddcbd
Ivan Kozak <Ivanpay8700@gmail.com>
2023-09-19 15:00:14 +0300

    Added the greedy algorithm method to initialize the scheduling prodecure on both of the sets

--- RunAlgorithm.py
+++ RunAlgorithm.py
@@ -4,15 +4,16 @@ from random import randint
 class RunAlgorithm(object):
     """ This class will contain the schedule on both of the sets"""
     
+    #Receiving the input from the user
     def __init__(self):
         self.list_of_jobs=[]
         input_option=int(input("Enter how would you like to receive the input? 0 - .txt file, 1 - manual insertion, 2 - randomization : "))
         if(input_option==0):
             input_from_text=open("inputs/input1.txt") #change the file name to read different input
-            parameter=int(input_from_text.readline())
+            self.parameter=int(input_from_text.readline())
             amount_of_jobs=int(input_from_text.readline())
         else:
-            parameter=int(input("Enter how many machines will be in the first set, the second set will contain twice as much machines : "))
+            self.parameter=int(input("Enter how many machines will be in the first set, the second set will contain twice as much machines : "))
             amount_of_jobs=int(input("Enter the total amount of jobs there will be in the input : "))
         indecies=[]
         jobs_values=[]
@@ -33,9 +34,63 @@ class RunAlgorithm(object):
             else:
                 jobs_values[(x-1)].append(randint(21,60))
                 jobs_values[(x-1)].append(randint(21,60))
-        list_of_jobs=dict(zip(indecies,jobs_values))
-        print(list_of_jobs)
+        self.list_of_jobs=dict(zip(indecies,jobs_values))
+        
+        self.schedule_first_set=[]
+        self.summation_array_first_set=[]
+        self.schedule_second_set=[]
+        self.summation_array_second_set=[]
+        for x in range (0,self.parameter):
+            self.schedule_first_set.append({})
+            self.summation_array_first_set.append(0)
+            self.schedule_second_set.append({})
+            self.schedule_second_set.append({})
+            self.summation_array_second_set.append(0)
+            self.summation_array_second_set.append(0)
+        self.greedy_algorithm()
+    
+    #Scheduling the jobs on the machines using the greedy algorithm - for each job, take the minimal cost of scheduling between the sets, then schedule it on the minimal machine at that set.
+    #if both values are equal, preferablly schedule the job on the second set (the bigger set)
+    def greedy_algorithm(self):
+        for key,value in self.list_of_jobs.items():
+            min_val=float('inf')
+            min_index=0
+            if(value[0]<value[1]):
+                for x in range(0,self.parameter):
+                    if(self.summation_array_first_set[x]<min_val):
+                        min_val=self.summation_array_first_set[x]
+                        min_index=x
+                self.schedule_first_set[min_index][key]=value
+                self.summation_array_first_set[min_index]+=value[0]
+            elif (value[0]>value[1]):
+                for x in range(0,self.parameter*2):
+                    if(self.summation_array_second_set[x]<min_val):
+                        min_val=self.summation_array_second_set[x]
+                        min_index=x
+                self.schedule_second_set[min_index][key]=value
+                self.summation_array_second_set[min_index]+=value[1]
+            else:
+                min_val2=float('inf')
+                min_index2=0
+                for x in range(0,self.parameter):
+                    if(self.summation_array_first_set[x]<min_val):
+                        min_val=self.summation_array_first_set[x]
+                        min_index=x
+                for x in range (0,2*self.parameter):
+                    if(self.summation_array_second_set[x]<min_val2):
+                        min_val2=self.summation_array_second_set[x]
+                        min_index2=x
+                if (min_val<min_val2):
+                    self.schedule_second_set[min_index][key]=value
+                    self.summation_array_second_set[min_index]+=value[0]
+                else:
+                    self.schedule_second_set[min_index2][key]=value
+                    self.summation_array_second_set[min_index2]+=value[1]
+                    
                 
+                
+            
+        
             
             
                 
